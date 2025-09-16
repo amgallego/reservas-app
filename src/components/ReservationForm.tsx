9037285d144f7310b2b55f0b7d1293bf6363ceab
AnaSofiaVelez078 <anadesarrollo01@gmail.com>
2025-09-16 17:47:16 -0500

    cambio2

--- src/components/ReservationForm.tsx
+++ src/components/ReservationForm.tsx
@@ -31,7 +31,6 @@ export default function ReservationForm({
         className="w-full border border-gray-300 rounded-lg px-3 py-2 mb-3 focus:ring-2 focus:ring-teal-500 focus:outline-none"
       />
 
-
        <input
         type="email"
         placeholder="Tu email"
